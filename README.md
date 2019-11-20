2d307e7852610e9921a1078bb7ec3315a3d31a97
Jasper van Rijbroek <jasper@jvar.nl>
2019-11-20 10:22:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# <Plugin_title>
+
+This plugin will add our own custom post types.  
+Per post type a separate init action will be added.
+
+This will keep the code clean.
---
+++ post-types.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: Custom posttypes for Brickson
+ * Plugin URI: https://brickson.nl
+ * Description: A plugin that will add all of our custom post types
+ * Version: 1.0.0
+ * Author: Brickson
+ * Author URI: https://brickson.nl
+ * License: MIT
+ */
+
+/**
+ * !!!Per post type a custom init action will be added so things keep clean!!!
+ */
+
+
+/**
+ * Add the teaser post type.
+ */
+add_action('init', function() {
+	register_post_type('teaser', [
+		'label' => __('Teasers'),
+		'labels' => [
+			'name' => __('Teasers'),
+			'singular_name' => __('Teaser'),
+			'add_new' => __('New teaser'),
+			'add_new_item' => __('Add new teaser'),
+			'edit_item' => __('Edit teaser'),
+			'new_item' => __('New teaser'),
+			'view_item' => __('View teaser'),
+			'view_items' => __('View teasers'),
+			'search_items' => __('Search teasers'),
+			'not_found' => __('No teasers found'),
+			'all_items' => __('All teasers')
+		],
+		'description' => __('Teaser posts'),
+		'public' => true,
+		'hierarchical' => true
+	]);
+});
